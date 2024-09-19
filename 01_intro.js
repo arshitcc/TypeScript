583959b794c6c288959696d499fa4612e4163a90
arshitcc <arshitcc@gmail.com>
2024-09-19 17:45:57 +0530

    initial commit : Start TypeScript

---
+++ 01_intro.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "Arshit",
+    age: 19
+};
+console.log("User");
---
+++ 01_intro.ts
@@ -0,0 +1,14 @@
+let user = {
+    name : "Arshit",
+    age : 19
+}
+
+console.log("User");
+// console.log(user.email);
+
+/* 
+    Syntax  
+    let variableName: type = value;
+*/
+
+export {}
